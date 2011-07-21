be616315877cac2aace148e8453b7714f59e3bcf
Jack Fong <jackfongcn@gmail.com>
2011-07-21 13:57:03 +0800

    move ColorConsole from NetworkMonitor.App to NetworkMonitor.Windows

---
+++ NetworkMonitor.Windows/Color.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkMonitor.Windows
+{
+    public enum Color
+    {
+        Black = 0x00000000,
+        DarkBlue = 0x00000001,
+        DarkGreen = 0x00000002,
+        DarkCyan = 0x00000003,
+        DarkRed = 0x00000004,
+        DarkPink = 0x00000005,
+        DarkYellow = 0x00000006,
+        DarkWhite = 0x00000007,
+        Gray = 0x00000008,
+        Blue = 0x00000009,
+        Green = 0x000000A,
+        Cyan = 0x0000000B,
+        Red = 0x0000000C,
+        Pink = 0x0000000D,
+        Yellow = 0x0000000E,
+        White = 0x0000000F
+    }
+}
---
+++ NetworkMonitor.Windows/ColorConsole.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace NetworkMonitor.Windows
+{
+    public class ColorConsole
+    {
+        private int hConsoleHandle;
+        private COORD ConsoleOutputLocation;
+        private CONSOLE_SCREEN_BUFFER_INFO ConsoleInfo;
+        private int OriginalColors;
+
+        private const int STD_OUTPUT_HANDLE = -11;
+
+        [DllImport("kernel32.dll", EntryPoint = "GetStdHandle", SetLastError = true,
+        CharSet = CharSet.Auto,
+        CallingConvention = CallingConvention.StdCall)]
+        private static extern int GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll", EntryPoint = "GetConsoleScreenBufferInfo",
+        SetLastError = true, CharSet = CharSet.Auto,
+        CallingConvention = CallingConvention.StdCall)]
+        private static extern int GetConsoleScreenBufferInfo(int hConsoleOutput,
+        ref CONSOLE_SCREEN_BUFFER_INFO lpConsoleScreenBufferInfo);
+
+        [DllImport("kernel32.dll", EntryPoint = "SetConsoleTextAttribute",
+        SetLastError = true, CharSet = CharSet.Auto,
+        CallingConvention = CallingConvention.StdCall)]
+        private static extern int SetConsoleTextAttribute(int hConsoleOutput, int wAttributes);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct COORD
+        {
+            short X;
+            short Y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct SMALL_RECT
+        {
+            short Left;
+            short Top;
+            short Right;
+            short Bottom;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct CONSOLE_SCREEN_BUFFER_INFO
+        {
+            public COORD dwSize;
+            public COORD dwCursorPosition;
+            public int wAttributes;
+            public SMALL_RECT srWindow;
+            public COORD dwMaximumWindowSize;
+        }
+
+        // Constructor.
+        public ColorConsole()
+        {
+            ConsoleInfo = new CONSOLE_SCREEN_BUFFER_INFO();
+            ConsoleOutputLocation = new COORD();
+            hConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+            GetConsoleScreenBufferInfo(hConsoleHandle, ref ConsoleInfo);
+            OriginalColors = ConsoleInfo.wAttributes;
+        }
+
+        public void TextColor(Color color)
+        {
+            SetConsoleTextAttribute(hConsoleHandle, (int)color);
+        }
+
+        public void TextColor(Color textColor, Color backColor)
+        {
+            SetConsoleTextAttribute(hConsoleHandle, (int)textColor + (int)backColor * 16);
+        }
+
+        public void ResetColor()
+        {
+            SetConsoleTextAttribute(hConsoleHandle, OriginalColors);
+        }
+    }
+}
